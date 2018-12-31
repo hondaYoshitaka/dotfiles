2c4e48fff4fe27798cf6a4e4b5744f2f7fe3f336
hondaYoshitaka <yhonda1001@gmail.com>
2018-12-31 15:45:41 +0900

    :wrench: add vimrc/gvimrc

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile
+
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/.gvimrc ~/.gvimrc
 
